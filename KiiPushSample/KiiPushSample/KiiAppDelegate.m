34a444737b74fdc2d11a6a97c6af7c9ec5406a38
Riza <riza.alaudin@kii.com>
2013-03-06 19:22:26 +0900

    initial

--- KiiPushSample/KiiPushSample/KiiAppDelegate.m
+++ KiiPushSample/KiiPushSample/KiiAppDelegate.m
@@ -7,15 +7,119 @@
 //
 
 #import "KiiAppDelegate.h"
-
+#import <KiiSDK/Kii.h>
 @implementation KiiAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [Kii beginWithID:@"65457c51"
+              andKey:@"80ad5019e0c96eb3b8b1ec293c29792e"
+             andSite:kiiSiteUS];
+    [self apnsAuth];
+    [self testSendExpicitPush];
+    //[Kii enableAPNSWithDevelopmentMode:YES andNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    
     // Override point for customization after application launch.
     return YES;
 }
-							
+- (void) auth {
+    
+    NSString *username = [NSString stringWithFormat:@"test%d", (int)round([[NSDate date] timeIntervalSince1970])];
+   
+    
+    KiiError *err;
+    KiiUser *user = [KiiUser userWithUsername:username andPassword:@"kii12345"];
+    [user performRegistrationSynchronous:&err];
+    [user describe];
+    
+    [user refreshSynchronous:&err];
+    [user describe];
+    
+    [user setObject:@"121320540207054848 " forKey:@"internalUserID"];
+    
+  
+    [user saveSynchronous:&err];
+    
+    
+    [user refreshSynchronous:&err];
+    [user describe];
+}
+
+-(void) apnsAuth{
+    NSError* error;
+    
+    KiiUser* user=[KiiUser authenticateSynchronous:@"test1360737152" withPassword:@"kii12345" andError:&error];
+    [user setObject:@"121320540207054848 " forKey:@"internalUserID"];
+    
+   
+    [user saveSynchronous:&error];
+    
+    
+    [user refreshSynchronous:&error];
+    [user describe];
+    
+    
+    
+}
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+	NSLog(@"My token is: %@", deviceToken);
+    
+    [Kii setAPNSDeviceToken:deviceToken];
+    // NSError* error;
+     //[KiiPushInstallation installSynchronous:&error];
+     
+    
+     /**/
+}
+
+- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
+{
+	NSLog(@"Failed to get token, error: %@", error);
+}
+- (void)application:(UIApplication*)application didReceiveRemoteNotification:(NSDictionary*)userInfo
+{
+	NSLog(@"Received notification: %@", userInfo);
+    KiiPushMessage* message=[KiiPushMessage messageFromAPNS:userInfo];
+    [message showMessageAlertWithTitle:@"test"];
+    
+    
+	
+}
+-(void) testSendExpicitPush{
+    KiiUser* user=[KiiUser currentUser];
+    
+    NSError* error=nil;
+    KiiTopic* topic=[user topicWithName:@"testTopics"];
+   // [topic saveSynchronous:&error];
+    [KiiPushSubscription subscribeSynchronous:topic withError:&error];
+    KiiAPNSFields* fields=[KiiAPNSFields createFields];
+    fields.alertBody=@"Test";
+    [fields setSpecificData:[NSDictionary dictionaryWithObject:@"this data is only for iOS" forKey:@"ios_data"]];
+    
+    KiiGCMFields* gcmFields=[KiiGCMFields createFields];
+    
+    [gcmFields setSpecificData:[NSDictionary dictionaryWithObject:@"this data is only for android " forKey:@"andro_data"]];
+    
+    KiiPushMessage* message=[KiiPushMessage composeMessageWithAPNSFields:fields andGCMFields:gcmFields];
+    
+    
+    NSDictionary* customData=[NSDictionary dictionaryWithObject:@"data 1" forKey:@"data1"];
+    
+    message.data=customData;
+    
+    
+    
+    [topic sendMessageSynchronous:message withError:&error];
+    
+    if (nil!=error) {
+        NSLog(@"%@",error);
+    }
+    
+    
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
